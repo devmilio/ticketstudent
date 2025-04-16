1ef9d26417c208b6ccaa115ff6e7333e11da11be
Emilio <emiliogagnonpro@gmail.com>
2025-04-15 21:16:02 -0400

    sync user with internal db

--- ticketstudent/convex/users.ts
+++ ticketstudent/convex/users.ts
@@ -13,6 +13,23 @@ export const updateUser = mutation({
         .query("users")
         .withIndex("by_user_id", (q) => q.eq("userId", userId))
         .first();
-    }
     
+    if(existingUser){
+        // Update existing user values
+        await ctx.db.patch(existingUser._id,{
+            name,
+            email,
+        });
+        return existingUser._id;
+    }
+    // Create new user
+    const newUserId = await ctx.db.insert("users", {
+        userId,
+        name,
+        email,
+        stripeConnectId: undefined,
+    });
+  
+    return newUserId;
+    }
 })
